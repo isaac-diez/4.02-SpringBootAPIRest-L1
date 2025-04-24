07317039746439147616b38cb9299fdfcfca7564
isaac-diez <isaac.diez@proton.me>
2025-04-24 13:36:44 +0200

    feat: first commit and h2 creation

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+CREATE TABLE FRUITS(id INT PRIMARY KEY, name VARCHAR(255));
+INSERT INTO FRUITS (id, name) VALUES (1, 'apple');
+INSERT INTO FRUITS (id, name) VALUES (2, 'strawberry');
+INSERT INTO FRUITS (id, name) VALUES (3, 'banana');
+INSERT INTO FRUITS (id, name) VALUES (4, 'lemon');
+INSERT INTO FRUITS (id, name) VALUES (5, 'orange');
